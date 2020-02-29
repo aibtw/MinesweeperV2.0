611868fca12da8486d0c9aeac7e824893560eae8
AIB <madhmadmad@hotmail.com>
2020-02-29 17:09:48 +0300

    Finished working on the show_board method

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 class Board:
     def __init__(self, rows, cols):
+        """Initialize the board and does operations in it"""
         self.rows = rows
         self.columns = cols
         self.board = []
         self.build()
 
     def build(self):
+        """Build the board as an array"""
         for i in range(self.rows):
             self.board.append([])
             for j in range(self.columns):
                 self.board[i].append(0)
 
     def show_board(self):
-        # print(self.board)
+        """Prints the board in a good layout"""
+        # print the header row
         print("  n  ||", end="")
         for c in range(self.columns):
             print("  {}  |".format(c + 1), end="")
@@ -21,6 +24,15 @@ class Board:
             print("=====+", end="")
         print("")
 
+        # print the rest of the rows
+        for r in range(self.rows):
+            print("  {}  ||".format(r+1), end="")
+            for c in range(self.columns):
+                print("  {}  |".format(self.board[r][c]), end="")
+            print("\n-----++", end="")
+            for c in range(self.columns):
+                print("-----+", end="")
+            print()
 
 
 class Player:
@@ -28,5 +40,5 @@ class Player:
         pass
 
 
-board = Board(2,6)
+board = Board(6,6)
 board.show_board()
